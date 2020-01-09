2be4edca3269f297e3ad29b7101ae7cc50486b8b
Kevin Wenger <wenger.kev@gmail.com>
2020-01-09 18:58:55 +0100

    clickable sponsors - close #3

--- src/svg/sponsors/liip.js
+++ src/svg/sponsors/liip.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Liip = () => (
-  <div className="logo" style={{ display: "flex" }}>
+  <a href="https://liip.ch" target="_blank" className="logo" style={{ display: "flex" }}>
     <svg height={35} className="logo__L" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.7 49">
       <path fill="var(--color-white)" d="M0.1,46.8h29.6v-9.7H11.2V2.5H0.4v44.3H0.1z M0.1,46.8h29.6v-9.7H11.2V2.5H0.4v44.3H0.1z"/>
     </svg>
@@ -13,7 +13,7 @@ const Liip = () => (
       <path fill="var(--color-white)"
             d="M3.4,2.5v44.3h10.8V31h6.6c9.8,0,15-7,15-14.2c0-7.3-5.3-14.2-15-14.2H3.4L3.4,2.5z M14.3,12.3 h6.1c3,0,4.7,2.2,4.7,4.5s-1.7,4.5-4.7,4.5h-6.1V12.3L14.3,12.3z"/>
     </svg>
-  </div>
+  </a>
 )
 
 
