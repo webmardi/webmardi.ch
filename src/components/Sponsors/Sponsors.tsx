e55757e39f9c25d3199de01634d0e4cbb9b0382c
Yann Gouffon <github@yago-art.ch>
2021-11-12 08:39:54 +0100

    Update src/components/Sponsors/Sponsors.tsx
    Co-authored-by: Kevin Wenger <wenger.kev@gmail.com>

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -32,7 +32,7 @@ const Sponsors = (): JSX.Element => {
             />
           </a>
           <a
-            href="https://jolicode.com/"
+            href="https://jolicode.ch/"
             target="_blank"
             rel="noreferrer"
             tw="flex items-center justify-center h-40 lg:h-60 bg-blue-dark transition-colors md:hover:bg-blue-light"
