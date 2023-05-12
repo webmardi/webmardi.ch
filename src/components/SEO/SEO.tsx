10d88eea7655de001150d84460383a8ae1e1efa5
Yann Gouffon <github@yago-art.ch>
2023-05-12 09:47:50 +0200

    🔧 add GA4 - #86

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -78,6 +78,22 @@ const SEO = ({ title, imageUrl, description }: Props): JSX.Element => {
             `,
         }}
       />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4PZVCNZ6D5"
+      />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4PZVCNZ6D5');
+            `,
+        }}
+      />
     </Head>
   );
 };
